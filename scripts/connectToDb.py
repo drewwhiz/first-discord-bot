0d9963e06e5c8ea65e2840d2834604ec93af3a92
Lawrence (Drew) Whisenant <drewwhis@users.noreply.github.com>
2019-08-05 21:23:07 -0500

    script to add event

---
+++ scripts/connectToDb.py
@@ -0,0 +1,26 @@
+import pymongo
+import json
+
+
+def connectToDb() -> pymongo.database.Database:
+    try:
+        authJsonFile = open('../auth/auth.json')
+    except:
+        print("Unable to locate the JSON auth file.")
+        return None
+
+    try:
+        data = json.load(authJsonFile)
+    except:
+        print("Auth file is not properly formatted JSON.")
+        return None
+
+    client = pymongo.MongoClient(
+        data["dbUri"],
+        username=data["mongoUserName"],
+        password=data["mongoPassword"],
+        authSource=data["dbName"]
+    )
+
+    db = client[data["dbName"]]
+    return db
