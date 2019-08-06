82543f8d3e1c12246ca2f81f7fecc86aed13c4fb
Lawrence (Drew) Whisenant <drewwhis@users.noreply.github.com>
2019-08-05 22:37:21 -0500

    create remove event script

---
+++ scripts/removeEvent.py
@@ -0,0 +1,11 @@
+import connectToDb
+
+db = connectToDb.connectToDb()
+if db is not None:
+    eventName = str(input("Enter the name of the event to delete: "))
+
+    if db.events.count_documents({"name": eventName}) > 0:
+        db.events.delete_one({"name": eventName})
+        print("Deleted event named %s." % (eventName))
+    else:
+        print("No event named %s found." % (eventName))
