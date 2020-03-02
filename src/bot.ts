445225e6ad55d0a9426893f7d0e588ecfbc344f6
Lawrence (Drew) Whisenant <drewwhis@users.noreply.github.com>
2020-03-01 20:45:32 -0600

    add team command

--- src/bot.ts
+++ src/bot.ts
@@ -7,6 +7,7 @@ import { MainGoalCommand } from "./commands/funCommands/MainGoalCommand";
 import { ManualCommand } from "./commands/funCommands/ManualCommand";
 import { LogCommand } from "./commands/LogCommand.js";
 import { DanceCommand } from "./commands/funCommands/DanceCommand.js";
+import { TeamCommand } from "./commands/frcCommands/TeamCommand.js";
 
 // Configure default logger settings
 configure({
@@ -22,7 +23,8 @@ const commands = [
   new GameCommand(),
   new ManualCommand(),
   new LogCommand(),
-  new DanceCommand()
+  new DanceCommand(),
+  new TeamCommand()
 ];
 
 // Connect
---
+++ src/commands/frcCommands/TeamCommand.ts
@@ -0,0 +1,20 @@
+import { ICommand } from "../ICommand";
+import { Message } from "discord.js";
+
+export class TeamCommand implements ICommand {
+  name: string = "team";
+  description: string = "Gets the URL of a team's Blue Alliance page for the current year.";
+
+  public trigger(message: Message): boolean {
+    let team = Number(message.content.trim());
+    return Number.isInteger(team) && team > 0;
+  }
+
+  public async execute(message: Message, args: string[]): Promise<void> {
+    let year = new Date().getFullYear();
+    let team = Number(message.content.trim());
+
+    let reply = `Check out Team ${team} this season on The Blue Alliance: https://www.thebluealliance.com/team/${team}/${year}`;
+    message.reply(reply);
+  }
+}
