df891c4887592e883060fbac4da222ea9665ce54
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2020-03-17 23:44:58 -0500

    update tsimfd command restrictions

--- src/commands/funCommands/TsimfdCommand.ts
+++ src/commands/funCommands/TsimfdCommand.ts
@@ -1,5 +1,5 @@
 import { ICommand } from "../ICommand";
-import { Message, TextChannel } from "discord.js";
+import { Message, TextChannel, Collection, Role } from "discord.js";
 import "../../extensions/StringExtension";
 
 export class TsimfdCommand implements ICommand {
@@ -14,7 +14,17 @@ export class TsimfdCommand implements ICommand {
 
     public async execute(message: Message, args: string[]): Promise<void> {
         if (this.isAsking(message.content)) {
-            message.author.send("https://www.youtube.com/watch?v=ij8CkMm2fPw");
+            if (message.guild === null) {
+                return;
+            }
+
+            let allowedRoles = message.guild.roles.filter(r => r.name === "mentor" || r.name === "alumni").map(r => r.id);
+
+            if (allowedRoles.map(r => message.member.roles.has(r)).length > 0) {
+                message.author.send("https://www.youtube.com/watch?v=ij8CkMm2fPw");
+            } else {
+                message.author.send("Sorry, you'll have to ask a mentor or an alum.");
+            }
 
         } else if (this.containsCoolOrEquivalent(message.content)) {
             message.channel.send(TsimfdCommand.TSIMFD);
