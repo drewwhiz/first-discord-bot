5fa0fb80bcc35225c922452563f60d001707a808
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2024-04-30 23:06:56 -0500

    add rtfm

--- src/commands/funCommands/ManualCommand.ts
+++ src/commands/funCommands/ManualCommand.ts
@@ -7,12 +7,20 @@ export class ManualCommand implements ICommand {
   public name: string = "manual";
   public description: string = "Tells people to read the manual.";
 
+  private getText(content: string): string {
+    const invariant = content.toLowerCase();
+    if (invariant.indexOf("rtm") >= 0) return "rtm";
+    if (invariant.indexOf("rtfm") >= 0) return "rtfm";
+    return null;
+  }
+
   public trigger(message: Message): boolean {
-    return message != null && message.content.containsAny("rtm");
+    return this.getText(message?.content) != null;
   }
 
   public async execute(message: Message, args: string[]): Promise<void> {
-    message.channel.send(
-      `In case ${message.author.displayName} wasn't clear, you should probably (R)EAD (T)HE (M)ANUAL!`);
+    const acronym = this.getText(message?.content);
+    const text = acronym === 'rtm' ? '(R)EAD (T)HE (M)ANUAL' : '(R)EAD (T)HE (F)IRST (M)ANUAL';
+    message.channel.send(`In case that wasn't clear, you should probably ${text}!`);
   }
 }
