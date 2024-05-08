ef9a144e06010c0082c38f8c3e25ad1e31789fbe
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2024-05-07 20:39:10 -0500

    only allow in guild

--- src/commands/calendarCommands/ListCalendarCommand.ts
+++ src/commands/calendarCommands/ListCalendarCommand.ts
@@ -18,13 +18,19 @@ export class ListCalendarCommand implements ICommand {
     }
 
     async execute(message: Message<boolean>): Promise<void> {
+        const member = message.member;
+        if (member == null) {
+            message.reply('Sorry, this action is not supported in this context.');
+            return;
+        }
+
         const calendars = await this._service.getAll();
         if (calendars.length == 0) {
             message.reply('We are not tracking any calendars at the moment!');
             return;
         }
 
-        let replyStarter = 'We are currently tracking these calendars:';
+        const replyStarter = 'We are currently tracking these calendars:';
         const lines = calendars.map(c => `\n- <${c.url}>`).join('');
         message.reply(replyStarter + lines);
     }
