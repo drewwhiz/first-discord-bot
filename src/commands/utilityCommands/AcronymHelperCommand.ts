affa8e2f821bd6903d0ce6800c7ecd41335988be
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2024-05-30 18:56:26 -0500

    add more acronyms

--- src/commands/utilityCommands/AcronymHelperCommand.ts
+++ src/commands/utilityCommands/AcronymHelperCommand.ts
@@ -15,14 +15,23 @@ export class AcronymHelperCommand implements ICommand {
 
   public trigger(message: Message): boolean {
     if (!message.content.trim().endsWith('?')) return false;
-    const content = message.content.stripPunctuation().trim();
-    return this._acronyms.get(content) != null;
+    let content = message.content.stripPunctuation().trim();
+    if (this._acronyms.get(content) != null) return true;
+    if (!content.toLowerCase().endsWith('s')) return false;
+    return this._acronyms.get(content.substring(0, content.length - 1)) != null;
   }
 
   public async execute(message: Message): Promise<void> {
-    const content = message.content.stripPunctuation().trim();
-    const acronym = await this._acronyms.get(content);
+    let content = message.content.stripPunctuation().trim();
+    let acronym = await this._acronyms.get(content);
+    if (acronym != null) {
+      await message.reply(acronym.explanation);
+      return;
+    }
+
+    if (!content.toLowerCase().endsWith('s')) return;
+    acronym = await this._acronyms.get(content.substring(0, content.length - 1));
     if (acronym == null) return;
-    await message.reply(acronym.explanation);
+    await message.reply(acronym.explanation + '(s)');
   }
 }
