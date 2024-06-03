5ea4f01ffc5cab49c3b1d005219b56f86a25a90d
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2024-06-02 23:24:23 -0400

    cleanup random

--- src/services/interfaces/IRandomNumberService.ts
+++ src/services/interfaces/IRandomNumberService.ts
@@ -1,12 +1,6 @@
-import { Message } from 'discord.js';
-
-export interface IRandomNumberService {
-    simpleRollReply(message: Message): Promise<void>;
-    simpleFlipReply(message: Message): Promise<void>;
-    countFlipReply(message: Message): Promise<void>;
-    runFlipsReply(message: Message, count: number): Promise<void>;
-    singleRollReply(message: Message, max: number): Promise<void>;
-    multipleRollReply(message: Message, max: number, count: number): Promise<void>;
-    countRollReply(message: Message): Promise<void>;
+export interface IRandomNumberService {    
     getSingleRoll(max: number): number;
+    getMultipleRolls(max: number, count: number): number[];
+    getSimpleRoll(): number;
+    isHeads(): boolean;
 }
