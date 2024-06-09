cfae948a7515f482e90878e957a76a34ba1f8996
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2024-06-08 23:41:11 -0500

    lol command

---
+++ test/commands/funCommands/LolCommand.test.ts
@@ -0,0 +1,35 @@
+import { Message } from 'discord.js';
+import { expect } from 'chai';
+import { LolCommand } from '../../../src/commands/funCommands/LolCommand.js';
+
+describe('Lol Command', function() {
+  it('should trigger on lol', function() {
+    const content = 'lol';
+    const message = Message.prototype;
+    message.content = content;
+    const command = new LolCommand();
+
+    const result = command.trigger(message);
+    expect(result).to.be.true;
+  });
+
+  it('should trigger on lol!', function() {
+    const content = 'lol!';
+    const message = Message.prototype;
+    message.content = content;
+    const command = new LolCommand();
+
+    const result = command.trigger(message);
+    expect(result).to.be.true;
+  });
+
+  it('should not trigger on lollipop', function() {
+    const content = 'lollipop';
+    const message = Message.prototype;
+    message.content = content;
+    const command = new LolCommand();
+
+    const result = command.trigger(message);
+    expect(result).to.be.false;
+  });
+});
