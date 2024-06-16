b9b7811fe3dc7aacc6463395b574c4e8c7440dcd
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2024-06-15 20:56:57 -0500

    update respects

--- src/commands/funCommands/RespectsCommand.ts
+++ src/commands/funCommands/RespectsCommand.ts
@@ -1,40 +1,40 @@
 import { ICommand } from '../ICommand.js';
 import { Message } from 'discord.js';
-import { Dictionary } from 'typescript-collections';
+import { ICooldownDataService } from '../../dataservices/interfaces/ICooldownDataService.js';
+import { DateTimeUtilities } from '../../utility/DateTimeUtilities.js';
 
 export class RespectsCommand implements ICommand {
-  private static COOLDOWN: number = 24 * 60 * 60 * 1000;
+  private static COOLDOWN_HOURS: number = 24; // one day
 
-  name: string = 'respect';
-  description: string = 'Press F to Pay Respects';
+  public readonly name: string = 'respect';
+  public readonly description: string = 'Press F to Pay Respects';
 
-  private lastRespects: Dictionary<string, Date> = new Dictionary<string, Date>();
+  private readonly _coolDowns: ICooldownDataService;
+
+  constructor(coolDowns: ICooldownDataService) {
+    this._coolDowns = coolDowns;
+  }
 
   trigger(message: Message): boolean {
-    if (message == null) return false;
-    const isF = message.content.stripPunctuation().trim().toLowerCase() == 'f';
-    if (!isF) return false;
-
-    const destination = message.channel?.id;
-    if (destination == null) return false;
-
-    const now = new Date();
-    const lastRespect = this.lastRespects.getValue(destination);
-    if (lastRespect == null) {
-      this.lastRespects.setValue(destination, now);
-      return true;
-    }
-
-    if (Math.abs(now.getTime() - lastRespect.getTime()) > RespectsCommand.COOLDOWN) {
-      this.lastRespects.setValue(destination, now);
-      return true;
-    }
-
-    return false;
+    return message.content.stripPunctuation().trim().toLowerCase() == 'f';
   }
 
   async execute(message: Message): Promise<void> {
-    message.channel.send({
+    let activeCooldown = await this._coolDowns.getByKeys(this.name, message.channelId);
+    if (activeCooldown == null) {
+      activeCooldown = {
+        id: 0,
+        commandName: this.name,
+        channelId: message.channelId,
+        deadline: null
+      };
+    }
+
+    if (DateTimeUtilities.isCooldownInEffect(activeCooldown.deadline)) return;
+    activeCooldown.deadline = DateTimeUtilities.getFutureTimeUTCString(RespectsCommand.COOLDOWN_HOURS);
+    await this._coolDowns.upsert(activeCooldown);
+
+    await message.channel.send({
       files: ['./img/respects.jpeg']
     });
   }
