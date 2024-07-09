cd8511cd7e7d1f87e4015e30501a8024169404b0
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2024-07-08 19:21:30 -0500

    add command (#92)

---
+++ test/commands/funCommands/EveryoneCommand.test.ts
@@ -0,0 +1,31 @@
+import { Message, MessageMentions } from 'discord.js';
+import { expect } from 'chai';
+import * as TypeMoq from 'typemoq';
+import { EveryoneCommand } from '../../../src/commands/funCommands/EveryoneCommand.js';
+
+
+describe('Everyone Command', function() {
+  it('should trigger on messages where everyone is tagged', function() {
+    const mentions = TypeMoq.Mock.ofType<MessageMentions>();
+    mentions.setup(m => m.everyone).returns(() => true);
+
+    const message = TypeMoq.Mock.ofType<Message>();
+    message.setup(m => m.mentions).returns(() => mentions.object);
+
+    const command = new EveryoneCommand();
+    const result = command.trigger(message.object);
+    expect(result).to.be.true;
+  });
+
+  it('should not trigger on messages where everyone is not tagged', function() {
+    const mentions = TypeMoq.Mock.ofType<MessageMentions>();
+    mentions.setup(m => m.everyone).returns(() => false);
+
+    const message = TypeMoq.Mock.ofType<Message>();
+    message.setup(m => m.mentions).returns(() => mentions.object);
+
+    const command = new EveryoneCommand();
+    const result = command.trigger(message.object);
+    expect(result).to.be.false;
+  });
+});
