84ea5d20cacc4355452babcb472ca8059beac3d9
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2024-07-11 22:27:55 -0500

    add phrases

--- test/commands/funCommands/BonkCommand.test.ts
+++ test/commands/funCommands/BonkCommand.test.ts
@@ -83,6 +83,46 @@ describe('Bonk Command', function() {
     expect(result).to.be.true;
   });
 
+  it('should trigger on \'FTC SWERVE\'', function() {
+    const content = 'FTC SWERVE';
+    const message = Message.prototype;
+    message.content = content;
+    const command = new BonkCommand();
+
+    const result = command.trigger(message);
+    expect(result).to.be.true;
+  });
+
+  it('should trigger on \'ftcswerve\'', function() {
+    const content = 'FTCSWERVE';
+    const message = Message.prototype;
+    message.content = content;
+    const command = new BonkCommand();
+
+    const result = command.trigger(message);
+    expect(result).to.be.true;
+  });
+
+  it('should trigger on \'ftc swervedrive\'', function() {
+    const content = 'ftc SWERVEDRIVE';
+    const message = Message.prototype;
+    message.content = content;
+    const command = new BonkCommand();
+
+    const result = command.trigger(message);
+    expect(result).to.be.true;
+  });
+
+  it('should trigger on \'ftcswervedrive\'', function() {
+    const content = 'ftcswervedrive';
+    const message = Message.prototype;
+    message.content = content;
+    const command = new BonkCommand();
+
+    const result = command.trigger(message);
+    expect(result).to.be.true;
+  });
+
   it('should not trigger on \'event\'', function() {
     const content = 'event';
     const message = Message.prototype;
@@ -135,6 +175,16 @@ describe('Bonk Command', function() {
 
   it('should not trigger on \'quadruple\'', function() {
     const content = 'quadruple';
+    const message = Message.prototype;
+    message.content = content;
+    const command = new BonkCommand();
+
+    const result = command.trigger(message);
+    expect(result).to.be.false;
+  });
+
+  it('should not trigger on \'ftc and swerve\'', function() {
+    const content = 'ftc or frc swerve drive';
     const message = Message.prototype;
     message.content = content;
     const command = new BonkCommand();
