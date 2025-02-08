ca1d5d10dcc223c392b40d30a599e0b8b2d4902f
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2025-02-08 12:14:56 -0600

    tag correctly

--- src/commands/funCommands/MichaelSaidCommand.ts
+++ src/commands/funCommands/MichaelSaidCommand.ts
@@ -7,12 +7,16 @@ export class MichaelSaidCommand extends MessageCommand {
   public readonly description: string = 'Responds to \'michael said\' in the Discord.';
   public override readonly isSilly: boolean = true;
 
-  public override messageTrigger(message: Message): boolean {
-    const invariant = message.content.toLowerCase().trim();
+  private wasMichael(invariant: string): boolean {
     return invariant.containsAnyPhrases(['michael said', 'benben said', '@ttgwinds2 said']);
   }
 
+  public override messageTrigger(message: Message): boolean {
+    const invariant = message.content.toLowerCase().trim();
+    return this.wasMichael(invariant);
+  }
+
   public override async execute(message: Message): Promise<void> {
-    message.reply('Yeah, @annastrutz - Michael said!');
+    message.reply('Yeah, <@!996616874467008573> - Michael said!');
   }
 }
