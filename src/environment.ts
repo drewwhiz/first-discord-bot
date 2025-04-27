a69ea40990dbe36f3d8d01e2cf0c07ad1f9e7929
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2025-04-27 13:53:04 -0500

    handle missing secrets

--- src/environment.ts
+++ src/environment.ts
@@ -1,16 +1,30 @@
 import fs from 'fs';
 
 export class Secrets {
-  public static readonly TOKEN: string = fs.readFileSync('/run/secrets/TOKEN', 'utf8').trim();
-  public static readonly CLIENT_ID: string = fs.readFileSync('/run/secrets/CLIENT_ID', 'utf8').trim();
-  public static readonly GUILD_ID: string = fs.readFileSync('/run/secrets/GUILD_ID', 'utf8').trim();
-  public static readonly WEATHER_API_KEY: string = fs.readFileSync('/run/secrets/WEATHER_API_KEY', 'utf8').trim();
-  public static readonly DEFAULT_ZIP: string = fs.readFileSync('/run/secrets/DEFAULT_ZIP', 'utf8').trim();
-  public static readonly RESTRICTED_CHANNEL: string = fs.readFileSync('/run/secrets/RESTRICTED_CHANNEL', 'utf8').trim();
-  public static readonly MOD_REPORT_CHANNEL: string = fs.readFileSync('/run/secrets/MOD_REPORT_CHANNEL', 'utf8').trim();
-  public static readonly SERIOUS_CHANNELS: string = fs.readFileSync('/run/secrets/SERIOUS_CHANNELS', 'utf8').trim();
-  public static readonly DATABASE: string = fs.readFileSync('/run/secrets/db_database', 'utf8').trim();
-  public static readonly DB_PASSWORD: string = fs.readFileSync('/run/secrets/db_password', 'utf8').trim();
-  public static readonly DB_HOST: string = fs.readFileSync('/run/secrets/db_host', 'utf8').trim();
-  public static readonly DB_PORT: number = parseInt(fs.readFileSync('/run/secrets/db_port', 'utf8').trim());
+  private static getSecretString(name: string): string {
+    try {
+      return fs.readFileSync(`/run/secrets/${name}`, 'utf8').trim();
+    } catch {
+      return '';
+    }
+  }
+
+  private static getSecretInteger(name: string): number {
+    const secretString = this.getSecretString(name);
+    if (secretString == '') return 0;
+    return parseInt(secretString);
+  }
+
+  public static readonly TOKEN: string = this.getSecretString('TOKEN');
+  public static readonly CLIENT_ID: string = this.getSecretString('CLIENT_ID');
+  public static readonly GUILD_ID: string = this.getSecretString('GUILD_ID');
+  public static readonly WEATHER_API_KEY: string = this.getSecretString('WEATHER_API_KEY');
+  public static readonly DEFAULT_ZIP: string = this.getSecretString('DEFAULT_ZIP');
+  public static readonly RESTRICTED_CHANNEL: string = this.getSecretString('RESTRICTED_CHANNEL');
+  public static readonly MOD_REPORT_CHANNEL: string = this.getSecretString('MOD_REPORT_CHANNEL');
+  public static readonly SERIOUS_CHANNELS: string = this.getSecretString('SERIOUS_CHANNELS');
+  public static readonly DATABASE: string = this.getSecretString('db_database');
+  public static readonly DB_PASSWORD: string = this.getSecretString('db_password');
+  public static readonly DB_HOST: string = this.getSecretString('db_host');
+  public static readonly DB_PORT: number = this.getSecretInteger('db_port');
 }
