c143f8a1e6e527d72b11f87a7f36ee3bae755c8b
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2026-05-14 23:14:14 -0500

    Feature/calendar upgrades (#176)
    * handle virtual and reset
    
    * fix week bounds
    
    * fix for nonreminders

--- src/commands/slashCommands/CalendarReportCommand.ts
+++ src/commands/slashCommands/CalendarReportCommand.ts
@@ -1,15 +1,18 @@
-import { ChatInputCommandInteraction, Client, GuildBasedChannel, GuildScheduledEventManager, Message, SlashCommandOptionsOnlyBuilder, TextChannel } from 'discord.js';
+import { ChatInputCommandInteraction, Client, GuildBasedChannel, GuildEmoji, GuildScheduledEventManager, Message, SlashCommandOptionsOnlyBuilder, TextChannel } from 'discord.js';
 import SlashCommand from '../SlashCommand.js';
 import { ITimeUnit } from '../../models/ITimeUnit.js';
+import { Nullable } from '../../models/Nullable.js';
 
 export default class CalendarReportCommand extends SlashCommand {
   private static readonly _DURATION: string = 'duration';
+  private static readonly _ATTENDANCE_REQUEST: string = 'attendance_request';
   private static readonly MENTOR_EMOJI = ':hammer:';
   private static readonly UNAVAILABLE_EMOJI = ':baby_chick:';
-  private static readonly STUDENT_EMOJI = ':robot:';
-  private static readonly DOZER_EMOJI_NAME = 'dozer';
+  private static readonly STUDENT_EMOJI_NAME = 'dozer';
+  private static readonly VIRTUAL_EMOJI = ':robot:';
 
-  private readonly _eventManager: GuildScheduledEventManager;
+  private readonly _eventManager: Nullable<GuildScheduledEventManager> = null;
+  private readonly _studentEmoji: Nullable<GuildEmoji> = null;
 
   public constructor(client: Client) {
     super('upcoming', 'List upcoming events');
@@ -17,6 +20,9 @@ export default class CalendarReportCommand extends SlashCommand {
     const guilds = client.guilds.cache.map(g => g);
     if (guilds == null || guilds.length != 1) return;
     this._eventManager = guilds[0].scheduledEvents;
+    const studentEmoji = guilds[0].emojis.cache.find(emoji => emoji.name === CalendarReportCommand.STUDENT_EMOJI_NAME);
+    if (studentEmoji == null) return;
+    this._studentEmoji = studentEmoji;
   }
 
   public override async build(): Promise<SlashCommandOptionsOnlyBuilder> {
@@ -32,13 +38,18 @@ export default class CalendarReportCommand extends SlashCommand {
             { name: 'day', value: ITimeUnit.DAY }
           )
           .setRequired(false))
+      .addBooleanOption(option =>
+        option
+          .setName(CalendarReportCommand._ATTENDANCE_REQUEST)
+          .setDescription('Do you want to be able to capture attendance?')
+          .setRequired(false)
+      )
     ;
   }
 
   private async buildMessage(time: ITimeUnit, requestAttendance: boolean): Promise<string[]> {
-    // Start midnight today
     const startDate = new Date();
-    startDate.setHours(0, 0, 0);
+    if (requestAttendance) startDate.setHours(23, 59, 59);
 
     let endDate = startDate;
     switch (time) {
@@ -48,13 +59,15 @@ export default class CalendarReportCommand extends SlashCommand {
     case ITimeUnit.YEAR: endDate = new Date(new Date(startDate).setFullYear(startDate.getFullYear() + 1)); break;
     }
 
+    endDate.setHours(23, 59, 59);
+
     const results = 
       this._eventManager == null 
         ? [] 
         : (await this._eventManager.fetch())
           .map(e => e)
-          .filter(e => e.scheduledStartAt >= startDate && e.scheduledStartAt < endDate)
-          .sort((a,b) => a.scheduledStartTimestamp - b.scheduledStartTimestamp);
+          .filter(e => e.scheduledStartAt != null && e.scheduledStartAt >= startDate && e.scheduledStartAt < endDate)
+          .sort((a,b) => (a.scheduledStartTimestamp ?? 0) - (b.scheduledStartTimestamp ?? 0));
     
     if (results.length == 0) {
       switch (time) {
@@ -72,10 +85,11 @@ export default class CalendarReportCommand extends SlashCommand {
     const timezone = new Date().toLocaleDateString(undefined, { day: '2-digit', timeZoneName: 'long' }).substring(4);
     let events = results.map(r => {
       const date = r.scheduledStartAt;
+      if (date == null) return null;
       const startString = `${date.toLocaleDateString('en-GB', dateOption)} at ${date.toLocaleTimeString('en-US', timeOption)}`;
       const locationString = r.entityMetadata?.location != null ? ` (${r.entityMetadata.location})` : '';
       return `- ${r.name}: ${startString}${locationString}`;
-    });
+    }).filter(e => e != null);
 
     let header = '';
 
@@ -97,8 +111,8 @@ export default class CalendarReportCommand extends SlashCommand {
       break;
     }
 
-    if (requestAttendance) {
-      header = `@everyone ${header}.\n\nFor team activities (like meetings or outreach events), make sure to react to **each** event with ${CalendarReportCommand.STUDENT_EMOJI} if you are attending (mentors use ${CalendarReportCommand.MENTOR_EMOJI}) or ${CalendarReportCommand.UNAVAILABLE_EMOJI} if you are **not** attending.`;
+    if (requestAttendance && this._studentEmoji != null) {
+      header = `@everyone ${header}.\n\nFor team activities (like meetings or outreach events), make sure to react to **each** event with <:${CalendarReportCommand.STUDENT_EMOJI_NAME}:${this._studentEmoji.id}> if you are attending (mentors use ${CalendarReportCommand.MENTOR_EMOJI}), ${CalendarReportCommand.UNAVAILABLE_EMOJI} if you are **not** attending, or ${CalendarReportCommand.VIRTUAL_EMOJI} if you will be attending virtually.`;
       events = events.map(e => e.substring(2).trim());
     } else {
       header = `${header}:`;
@@ -110,34 +124,42 @@ export default class CalendarReportCommand extends SlashCommand {
   
   public async execute(interaction: ChatInputCommandInteraction): Promise<void> {
     let timeUnit = interaction.options.getNumber(CalendarReportCommand._DURATION) as ITimeUnit;
+    const sendAsAttendance = interaction.options.getBoolean(CalendarReportCommand._ATTENDANCE_REQUEST, false) == true;
     if (timeUnit == null) timeUnit = ITimeUnit.WEEK;
 
     await interaction.deferReply();
 
-    const response = await this.buildMessage(timeUnit, false);
-    if (response.length == 1) {
-      await interaction.followUp(response[0]);
+    const response = await this.buildMessage(timeUnit, sendAsAttendance);
+    if (response.length == 1 && !sendAsAttendance) {
+      await interaction.editReply(response[0]);
       return;
     }
 
-    await CalendarReportCommand.sendMessages(interaction, response);
+    if (!sendAsAttendance) {
+      await CalendarReportCommand.sendMessages(interaction, response);
+    } else {
+      await this.sendAsReminder(interaction, response);
+    }
   }
 
-  private static async reply(previousMessage: ChatInputCommandInteraction | Message, reply: string, textChannel: TextChannel): Promise<ChatInputCommandInteraction | Message> {
-    if (previousMessage == null) {
+  private static async reply(previousMessage: Nullable<ChatInputCommandInteraction | Message>, reply: string, textChannel: Nullable<TextChannel>): Promise<Nullable<ChatInputCommandInteraction | Message>> {
+    if (previousMessage == null && textChannel != null) {
       return await textChannel.send(reply);
     }
 
     const asInteraction = previousMessage as ChatInputCommandInteraction;
-    if (!asInteraction) return await previousMessage.reply(reply) as Message;
+    if (!asInteraction) {
+      if (previousMessage == null) return null;
+      return await previousMessage.reply(reply) as Message;
+    }
     
     await asInteraction.followUp(reply);
     return asInteraction;
   }
 
-  private static async sendMessages(firstMessage: ChatInputCommandInteraction, response: string[], textChannel: TextChannel = null): Promise<void> {
+  private static async sendMessages(firstMessage: ChatInputCommandInteraction, response: string[], textChannel: Nullable<TextChannel> = null): Promise<void> {
     const lines = response.length;
-    let previousMessage: ChatInputCommandInteraction | Message = firstMessage;
+    let previousMessage: Nullable<ChatInputCommandInteraction | Message> = firstMessage;
     let reply = response[0];
 
     for (let i = 1; i < lines; i++) {
@@ -174,6 +196,16 @@ export default class CalendarReportCommand extends SlashCommand {
     }
   }
 
+  private async sendAsReminder(firstMessage: ChatInputCommandInteraction, response: string[]): Promise<void> {
+    for (let i = 0; i < response.length; i++) {
+      if (i == 0) {
+        await firstMessage.editReply(response[i]);
+      } else {
+        await firstMessage.followUp(response[i]);
+      }
+    }
+  }
+
   public async sendReminder(channels: GuildBasedChannel[]): Promise<void> {
     const response: string[] = await this.buildMessage(ITimeUnit.WEEK, true);
 
@@ -181,21 +213,14 @@ export default class CalendarReportCommand extends SlashCommand {
       const textChannel = c as TextChannel;
       if (textChannel == null) return;
 
-      const customEmoji = c.guild.emojis.cache.find(emoji => emoji.name === CalendarReportCommand.DOZER_EMOJI_NAME);
-      let message: Message = null;
+      let message: Nullable<Message> = null;
       for (let i = 0; i < response.length; i++) {
         if (message != null) {
           message = await message.reply(response[i]);
           continue;
         }
 
-        const content = customEmoji == null
-          ? response[i]
-          : response[i].replace(
-            CalendarReportCommand.STUDENT_EMOJI,
-            `<:${CalendarReportCommand.DOZER_EMOJI_NAME}:${customEmoji.id}>`
-          );
-        message = await textChannel.send(content);
+        message = await textChannel.send(response[i]);
       }
     });
   }
