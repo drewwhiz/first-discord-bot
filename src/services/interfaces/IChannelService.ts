de2cc80018c5b1fa27995c15f51948431dd584ff
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2026-08-26 00:35:46 -0500

    handle threads (#210)

--- src/services/interfaces/IChannelService.ts
+++ src/services/interfaces/IChannelService.ts
@@ -2,4 +2,5 @@ import { GuildBasedChannel } from 'discord.js';
 
 export interface IChannelService {
   isSillyAllowed(channel: GuildBasedChannel): boolean;
+  notifyOnThreadCreation(channel: GuildBasedChannel): boolean;
 }
